13876795edda25070b2ba46a12c98910778d6358
Lucio Pintanel <lm.pintanel@gmail.com>
2021-11-27 20:25:13 -0300

    Created project

---
+++ spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/SpringIonicApplication.java
@@ -0,0 +1,14 @@
+package com.lucioPintanel.spring_ionic;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+
+}
