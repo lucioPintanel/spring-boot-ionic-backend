a9ae78bdcfeb0c9b65e9275a5ed255b9d6bf6b20
Lucio Pintanel <lm.pintanel@gmail.com>
2021-11-30 19:13:37 -0300

    Criando operação de instanciação

--- spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/SpringIonicApplication.java
+++ spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/SpringIonicApplication.java
@@ -1,14 +1,35 @@
 package com.lucioPintanel.spring_ionic;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringIonicApplication {
+import com.lucioPintanel.spring_ionic.domain.Categoria;
+import com.lucioPintanel.spring_ionic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringIonicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository cat_repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		cat_repo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
