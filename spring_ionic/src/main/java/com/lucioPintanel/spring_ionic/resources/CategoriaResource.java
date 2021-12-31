96e40710b895f8be34ae66c54aeb720dcce7adc7
Lucio Pintanel <lm.pintanel@gmail.com>
2021-12-31 17:05:40 -0300

    Atualizando Categoria com Put, outros pequenos ajustes

--- spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/resources/CategoriaResource.java
+++ spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.lucioPintanel.spring_ionic.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucioPintanel.spring_ionic.domain.Categoria;
 import com.lucioPintanel.spring_ionic.services.CategoriaService;
@@ -18,10 +22,24 @@ public class CategoriaResource {
 	private CategoriaService serv;
 	
 	@RequestMapping(value = "/{id}" ,method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
 		Categoria obj = serv.find(id);		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = serv.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}" ,method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = serv.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 
 }
