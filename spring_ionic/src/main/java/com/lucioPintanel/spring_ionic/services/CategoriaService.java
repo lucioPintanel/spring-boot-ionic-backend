b2485923ed978ae8ebd966e00c4941e3ea11ced6
Lucio Pintanel <lm.pintanel@gmail.com>
2021-12-31 16:35:22 -0300

    Revert "Inserindo uma nova Categoria com Post"
    This reverts commit 4b0a5493cad598b339c421659fd0bcc90deb69a5.

--- spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/services/CategoriaService.java
+++ spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/services/CategoriaService.java
@@ -21,9 +21,4 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado!" + "Id: " + id + " " + "Tipo: " + Categoria.class.getName()));
 	}
-	
-	public Categoria insert(Categoria obj) {
-		obj.setId(null);
-		return repo.save(obj);
-	}
 }
