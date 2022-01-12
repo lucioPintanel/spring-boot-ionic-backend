f4dac28a7b971d97f37f51949d199d76ec567693
Lucio Pintanel <lm.pintanel@gmail.com>
2022-01-11 21:21:35 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/services/validation/ClienteInsertValidator.java
+++ spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.lucioPintanel.spring_ionic.domain.Cliente;
 import com.lucioPintanel.spring_ionic.domain.enums.TipoCliente;
 import com.lucioPintanel.spring_ionic.dto.ClienteNewDTO;
+import com.lucioPintanel.spring_ionic.repositories.ClienteRepository;
 import com.lucioPintanel.spring_ionic.resources.exceptions.FieldMessage;
 import com.lucioPintanel.spring_ionic.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+		}
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente!"));
 		}
 
 		for (FieldMessage e : list) {
