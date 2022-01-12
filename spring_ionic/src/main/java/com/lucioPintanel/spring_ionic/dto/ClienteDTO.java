73f95f917ff2eed126201ee22aabf41fad3cbfcd
Lucio Pintanel <lm.pintanel@gmail.com>
2022-01-11 21:50:31 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/dto/ClienteDTO.java
+++ spring_ionic/src/main/java/com/lucioPintanel/spring_ionic/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.lucioPintanel.spring_ionic.domain.Cliente;
+import com.lucioPintanel.spring_ionic.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
